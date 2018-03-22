4bb051dbbc4885dde8c839b70d96763f41469666
Jada Washington <girlwithnerd@gmail.com>
2018-03-21 20:56:57 -0400

    Created Landing page and added home page

--- word-count/src/App.js
+++ word-count/src/App.js
@@ -2,12 +2,23 @@ import React, { Component } from 'react'
 import './App.css'
 
 import Landing from './Landing'
+import Home from './Home'
+
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Landing />
+        <Router>
+          <div>
+            <Route exact path='/' component={Landing} />
+            <Route exact path='/home' component={Home} />
+          </div>
+        </Router>
       </div>
     );
   }
--- word-count/src/Landing.js
+++ word-count/src/Landing.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react'
 import './Landing.css'
+import background from './image-1.jpg'
 
 class Landing extends Component {
   render() {
     return (
       <div className="Landing">
         <div className='row'>
-          <div className='col-md-7 left-landing'>
-            <h1 className='landing-head landing-one'>Write Up</h1>
-            <h3 className='landing-one'>Meet your writing goals by just writing</h3>
-            <button type="button" className="btn btn-outline-light">Join Now</button>
+          <div className='col-md-6 background'>
+            <div className='row'>
+              <div className='col-md-12 left-landing'>
+                <h1 className='landing-head landing-one'>upWord</h1>
+                <h3 className='landing-one mb1'>Meet your writing goals by just writing</h3>
+                <button type="button" className="btn btn-outline-light">Join Now</button>
+              </div>
+            </div>
           </div>
 
-          <div className='col-md-5 right-landing'>
-            <h1 className='landing-head'>Login</h1>
+          <div className='col-md-6 right-landing'>
+            <h1 className='landing-head landing-two'>Login</h1>
             <form>
               <input className='form-control' type="text" placeholder="Username" />
               <input className='form-control' type="text" placeholder="Password" />
